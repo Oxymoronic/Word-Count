42324504a58ca9f139b7bbaa1d3164af2d73c24c
Dee Corcoran <Oxymoronic2012@gmail.com>
2017-08-13 21:14:38 -0400

    add report function
    Added input prompt to read a file and a function called 'report' which prints out the result of the program.

--- word-count.py
+++ word-count.py
@@ -13,11 +13,45 @@ that each occurred.
 
 
 import re
+import operator
 
-def addFile(text):
+
+
+def addFile(text, dict):
     """Tokenizes a blob of text into individual words, which
     are delimited by any characters other than letters or
     numbers."""
     
     text = text.lower()
-    words = re.split("[^a-z0-9']+", text)
+    wordArray = re.split("[^a-z0-9']+", text)
+    for word in wordArray:
+        if word not in dict:
+            dict[word] = 1
+        else:
+            dict[word] += 1
+    return dict
+
+
+
+def report(dict):
+    sortedDict = sorted(d.items(),key=operator.itemgetter(1))
+    n = 0
+    while n < 10:
+        entry = sortedDict[n]
+        print(entry[0] + ":", entry[1])
+        n += 1
+
+
+
+def main():
+    dict = {}
+    fileName = ""
+    print("Please enter file name(s). Type 'stop' to end program")
+    while fileName != "stop":
+        fileName = input("File name: ")
+        myFile = open(fileName, 'r')
+        text = myFile.read().replace('\n', ' ')
+        dict = addFile(text, dict)
+    report(dict)
+
+
